3f898f722b47a87b28c49762f6dca84629e90f8c
joseignaciomg <josemorquillas@outlook.com>
2021-01-05 23:47:59 +0100

    commit3

--- Sacar los 3 mayores de un array.js
+++ Sacar los 3 mayores de un array.js
@@ -2,10 +2,10 @@
 
 // ordenas de mayor a menor, el mayor es el primero, y luego con slice sacas los 3 primeros. 
 function tresmayores(arr){
-    arr.sort((a,b) => b-a)arr.slice(0,3);
+    arr.sort((a,b) => b-a).slice(0,3);
 }
 
 //  ordenas de menor a mayor, y luego con slice sacas un nuevo array con los 3 ultimos argumentos del array.   
 function tresmayores(arr){
-    arr.sort((a,b) => a-b)arr.slice(arr.lenght-3,arr.lenght);
+    arr.sort((a,b) => a-b).slice(arr.lenght - 3,arr.lenght);
 }
