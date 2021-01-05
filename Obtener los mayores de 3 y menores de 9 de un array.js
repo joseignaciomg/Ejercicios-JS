b7718e74bbcb7b47ad6040b7067517960cf45078
joseignaciomg <josemorquillas@outlook.com>
2021-01-05 18:40:28 +0100

    commit

---
+++ Obtener los mayores de 3 y menores de 9 de un array.js
@@ -0,0 +1,8 @@
+function mayorde3ymenorde9(array){
+    for (let index = 0; index < array.length; index++) {
+    if (array[index]>3 && array[index]<9) {
+        console.log(array[index]);      
+    }
+}
+}
+console.log(mayorde3ymenorde9(array))
