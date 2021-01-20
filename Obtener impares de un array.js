dbbc09c8fbae171af05557b04238282568800782
joseignaciomg <josemorquillas@outlook.com>
2021-01-20 11:33:24 +0100

    commit

---
+++ Obtener impares de un array.js
@@ -0,0 +1,9 @@
+function pares(array){
+    for (let index = 0; index < array.length; index++) {
+        if(array[index]%2 !== 0){
+            console.log(array[index])
+        }
+        
+    }
+}
+console.log(pares(numeros))
